45c8c01a47eb5e10f589e7e132cd8ecf6c3dc831
mcapuccini <marco.capuccini@riken.jp>
2020-06-10 14:45:30 +0000

    test cmd

--- bin/bodytrack_trace.sh
+++ bin/bodytrack_trace.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cmd="/opt/parsec-3.0/pkgs/apps/bodytrack/inst/-.gcc-serial/bin/bodytrack /opt/parsec-3.0/pkgs/apps/bodytrack/run/sequenceB_1 4 1 1000 5 0 1" # simsmall
+# cmd="/opt/parsec-3.0/pkgs/apps/bodytrack/inst/-.gcc-serial/bin/bodytrack /opt/parsec-3.0/pkgs/apps/bodytrack/run/sequenceB_1 4 1 5 1 0 1" # test
 
 docker run -d \
     --name bodytrack \
