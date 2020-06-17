5421878456fc51260fb13952cc78a304cb9c5377
mcapuccini <marco.capuccini@riken.jp>
2020-06-17 13:38:37 +0000

    modular trace bin

--- bin/trace.sh
+++ bin/trace.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# TRACE_CMD (canneal)
-TRACE_CMD="/opt/parsec-3.0/pkgs/kernels/canneal/inst/amd64-linux.gcc-serial/bin/canneal 1 10000 2000 /opt/parsec-3.0/pkgs/kernels/canneal/run/100000.nets 32" # simsmall
-
 # Check env
 if [ -z "$TRACE_OUTDIR" ]; then 
     echo "TRACE_OUTDIR is unset"
@@ -16,11 +13,15 @@ if [ -z "$TRACE_CMD" ]; then
     echo "TRACE_CMD is unset"
     exit 1
 fi
+if [ -z "$TRACE_BENCH" ]; then 
+    echo "TRACE_BENCH is unset"
+    exit 1
+fi
 
 # Run roitrace
 docker run -d \
-    --name $(basename $TRACE_OUTDIR) \
-    -v $TRACE_OUTDIR:/out \
+    --name $TRACE_BENCH \
+    -v ${TRACE_OUTDIR}/${TRACE_BENCH}:/out \
     -v $TRACE_PARSECDIR:/opt/parsec-3.0 \
     -e LD_LIBRARY_PATH=/opt/parsec-3.0/pkgs/libs/hooks/inst/amd64-linux.gcc-serial/lib \
     --security-opt seccomp=unconfined \
