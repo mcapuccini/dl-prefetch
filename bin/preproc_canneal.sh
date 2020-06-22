b2fad29c17052d7d9d2deae5196a14015db95ff4
mcapuccini <marco.capuccini@riken.jp>
2020-06-22 09:16:43 +0000

    refactory

--- bin/preproc_canneal.sh
+++ bin/preproc_canneal.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set env
 export TRACE_BENCH="canneal"
 export TRACE_CMD="/opt/parsec-3.0/pkgs/kernels/canneal/inst/amd64-linux.gcc-serial/bin/canneal 1 15000 2000 /opt/parsec-3.0/pkgs/kernels/canneal/run/400000.nets 128" # simlarge
 # TRACE_CMD="/opt/parsec-3.0/pkgs/kernels/canneal/inst/amd64-linux.gcc-serial/bin/canneal 1 10000 2000 /opt/parsec-3.0/pkgs/kernels/canneal/run/100000.nets 32" # simsmall
+
+# Run
+bin/run_preprocessing.sh
