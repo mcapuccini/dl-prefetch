be3855873c6e2ae944c4eb79bcff5086be060402
mcapuccini <marco.capuccini@riken.jp>
2020-07-07 14:20:19 +0000

    add canneal trace

---
+++ bin/trace_canneal.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+export TRACE_BENCH="canneal"
+export TRACE_CMD="/opt/parsec-3.0/pkgs/kernels/canneal/inst/amd64-linux.gcc-serial/bin/canneal 1 15000 2000 /opt/parsec-3.0/pkgs/kernels/canneal/run/400000.nets 128" # simlarge
+# TRACE_CMD="/opt/parsec-3.0/pkgs/kernels/canneal/inst/amd64-linux.gcc-serial/bin/canneal 1 10000 2000 /opt/parsec-3.0/pkgs/kernels/canneal/run/100000.nets 32" # simsmall
+
+bin/trace.sh
