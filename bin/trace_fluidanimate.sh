49e0029afca7baf39bd666b4368bea81484bb82a
mcapuccini <marco.capuccini@riken.jp>
2020-07-30 07:24:48 +0000

    typo

--- bin/trace_fluidanimate.sh
+++ bin/trace_fluidanimate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set env
-export TRACE_BENCH="fuildanimate"
+export TRACE_BENCH="fluidanimate"
 input_base="/opt/parsec-3.0/pkgs/apps/fluidanimate/run"
 export TRACE_CMD="/opt/parsec-3.0/pkgs/apps/fluidanimate/inst/amd64-linux.gcc-serial/bin/fluidanimate 1 5 $input_base/in_300K.fluid $input_base/out.fluid" # simlarge
 
