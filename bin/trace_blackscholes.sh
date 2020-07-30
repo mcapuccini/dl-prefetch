d1fb3149aac305f3469bfc422860f309e08cfd51
mcapuccini <marco.capuccini@riken.jp>
2020-07-30 07:20:56 +0000

    typo

--- bin/trace_blackscholes.sh
+++ bin/trace_blackscholes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set env
-export TRACE_BENCH="blacksholes"
+export TRACE_BENCH="blackscholes"
 input_base="/opt/parsec-3.0/pkgs/apps/blackscholes/run"
 export TRACE_CMD="/opt/parsec-3.0/pkgs/apps/blackscholes/inst/amd64-linux.gcc-serial/bin/blackscholes 1 $input_base/in_64K.txt $input_base/prices.txt" # simlarge
 
