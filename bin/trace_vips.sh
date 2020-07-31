f0831e582f7dac3c4d6e386d8681c9bcce7dfd7d
mcapuccini <marco.capuccini@riken.jp>
2020-07-31 12:52:15 +0000

    using version 2.1 for vips

--- bin/trace_vips.sh
+++ bin/trace_vips.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-# This tool does not have ROI marked
+# This bench does not have ROI marked in version 3, using 2.1
 # https://lists.cs.princeton.edu/pipermail/parsec-users/2015-October/001681.html
 
 # Set env
 export TRACE_BENCH="vips"
-input_base="/opt/parsec-3.0/pkgs/apps/vips/run"
-export TRACE_CMD="/opt/parsec-3.0/pkgs/apps/vips/inst/amd64-linux.gcc-hooks/bin/vips im_benchmark $input_base/bigben_2662x5500.v $input_base/output.v" # simlarge
+input_base="/opt/parsec-2.1/pkgs/apps/vips/run"
+export TRACE_CMD="/opt/parsec-2.1/pkgs/apps/vips/inst/amd64-linux.gcc-serial/bin/vips im_benchmark $input_base/vulture_2336x2336.v $input_base/output.v" # simmedium
 
-# Run
-bin/trace.sh
+# Check env
+if [ -z "$TRACE_OUTDIR" ]; then 
+    echo "TRACE_OUTDIR is unset"
+    exit 1
+fi
+if [ -z "$TRACE_PARSECDIR_2" ]; then 
+    echo "TRACE_PARSECDIR_2 is unset"
+    exit 1
+fi
+
+# Run roitrace
+docker run -d \
+    --name $TRACE_BENCH \
+    -v ${TRACE_OUTDIR}/${TRACE_BENCH}:/out \
+    -v $TRACE_PARSECDIR_2:/opt/parsec-2.1 \
+    -e LD_LIBRARY_PATH=/opt/parsec-2.1/pkgs/libs/hooks/inst/amd64-linux.gcc-serial/lib \
+    --security-opt seccomp=unconfined \
+    -w /out \
+    mcapuccini/dl-prefetch sh -c \
+    "pin -t roitrace.so -- $TRACE_CMD"
