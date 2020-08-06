268b7bfed3a6f0343934645a0e493f609ab4b4c6
mcapuccini <marco.capuccini@riken.jp>
2020-08-06 13:25:22 +0000

    improve test

--- scripts/roitrace_test.py
+++ scripts/roitrace_test.py
@@ -6,7 +6,7 @@ import click
 @click.option('--bin-path', default='data/canneal_test/roitrace.bin')
 @click.option('--text-path', default='data/canneal_test/roitrace.txt')
 def roitrace_test(bin_path, text_path):
-  trace_bin = np.fromfile(bin_path, dtype=np.uint64)
+  trace_bin = np.fromfile(bin_path, dtype=np.int64)
   trace_txt_hex = pd.read_csv(text_path, header=None)[0].to_numpy()
   trace_txt = np.array([int(h, 16) for h in trace_txt_hex])
   assert((trace_bin == trace_txt).all())
