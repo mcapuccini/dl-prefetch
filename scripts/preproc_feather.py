131eea432cb82c4002d0ea13b6e4568e9b3cb470
mcapuccini <marco.capuccini@riken.jp>
2020-08-18 12:23:45 +0000

    remove deltas from feather packing

--- scripts/preproc_feather.py
+++ scripts/preproc_feather.py
@@ -7,18 +7,16 @@ import pandas as pd
 @click.option('--dataset-dir', required=True)
 @click.option('--limit/--no-limit', default=False)
 def preproc_feather(dataset_dir, limit):
-  # Load data and compute deltas
+  # Load data
   if limit:
     count=int(1e9)
   else:
     count=-1
   trace = np.fromfile(f'{dataset_dir}/roitrace.bin', dtype=np.int64, count=count)
   pc = np.fromfile(f'{dataset_dir}/pc.bin', dtype=np.int64, count=count)
-  deltas = trace[1:] - trace[:-1]
   # Store as feather
-  to_store = pd.DataFrame(trace[1:], columns=['addr'])
-  to_store['pc'] = pc[1:]
-  to_store['deltas'] = deltas
+  to_store = pd.DataFrame(trace, columns=['addr'])
+  to_store['pc'] = pc
   to_store.to_feather(f'{dataset_dir}/trace.feather')
 
 if __name__ == '__main__':
