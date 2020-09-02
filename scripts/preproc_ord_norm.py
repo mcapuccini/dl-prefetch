31951ca16272571b71fc2dd0ca9ac2e8defab258
mcapuccini <marco.capuccini@riken.jp>
2020-09-02 10:44:04 +0000

    ordinal encoding with dict

--- scripts/preproc_ord_norm.py
+++ scripts/preproc_ord_norm.py
@@ -1,40 +1,21 @@
 # Imports
-import shutil
-from os import path
-from tempfile import mkdtemp
-
 import click
 import numpy as np
-from joblib import Parallel, delayed, dump, load, parallel_backend
 from tqdm import tqdm
 
-def encode_obj(idx, obj, unique, out):
-  encoded = np.abs(obj - unique).argmin()
-  out[idx] = encoded
-
-def encode(to_encode, unique, n_jobs, backend):
-  # Create mmap for dumping output
-  tmp_folder = mkdtemp()
-  out_mmap_f = path.join(tmp_folder, 'encoding.mmap')
-  out_mmap = np.memmap(out_mmap_f, dtype=np.int64, shape=len(to_encode), mode='w+')
-
-  # Dump unique to mmap
-  unique_mmap_f = path.join(tmp_folder, 'unique.mmap')
-  dump(unique, unique_mmap_f)
-  unique_mmap = load(unique_mmap_f, mmap_mode='r')
-
-  # Encode in parallel
+def encode(to_encode, unique_to_index, train_ord):
+  to_ret = np.zeros(len(to_encode), dtype=np.int64)
   p_bar = tqdm(np.ndenumerate(to_encode), desc='Encoding', total=len(to_encode))
-  with parallel_backend(backend, n_jobs=n_jobs):
-    Parallel()(delayed(encode_obj)(idx, obj, unique_mmap, out_mmap) for idx, obj in p_bar)
-
-  return out_mmap, tmp_folder
+  for idx, obj in p_bar:
+    try:
+      to_ret[idx] = train_ord[unique_to_index[obj]]
+    except KeyError:
+      to_ret[idx] = len(unique_to_index)
+  return to_ret
 
 @click.command()
 @click.option('--dataset-dir', required=True)
-@click.option('--n-jobs', default=1, type=int)
-@click.option('--backend', default='loky')
-def preproc_ord_norm(dataset_dir, n_jobs, backend):
+def preproc_ord_norm(dataset_dir):
   # Load data
   data = np.load(f'{dataset_dir}/deltas_split.npz')
   train = data['train']
@@ -42,14 +23,15 @@ def preproc_ord_norm(dataset_dir, n_jobs, backend):
   test = data['test']
 
   # Ordinal encoding
-  train_unique, train_ord = np.unique(train, return_inverse=True)
-  dev_ord, tmpf_dev = encode(dev, train_unique, n_jobs, backend)
-  test_ord, tmpf_test = encode(test, train_unique, n_jobs, backend)
+  train_unique, train_index, train_ord = np.unique(train, return_index=True, return_inverse=True)
+  unique_to_index = dict(np.stack((train_unique, train_index)).T)
+  dev_ord = encode(dev, unique_to_index, train_ord)
+  test_ord = encode(test, unique_to_index, train_ord)
 
   # Normalization
-  train_norm = train_ord / (len(train_unique) - 1)
-  dev_norm = dev_ord / (len(train_unique) - 1)
-  test_norm = test_ord / (len(train_unique) - 1)
+  train_norm = train_ord / len(train_unique)
+  dev_norm = dev_ord / len(train_unique)
+  test_norm = test_ord / len(train_unique)
 
   # Store
   np.savez_compressed(
@@ -59,10 +41,6 @@ def preproc_ord_norm(dataset_dir, n_jobs, backend):
     test_norm=test_norm.astype(np.float32),
     train_unique=train_unique,
   )
-
-  # Cleanup
-  shutil.rmtree(tmpf_dev)
-  shutil.rmtree(tmpf_test)
 
 if __name__ == '__main__':
   preproc_ord_norm() # pylint: disable=no-value-for-parameter
